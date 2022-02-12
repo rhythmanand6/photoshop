422ec99756352095e8e96d100bda97ff58f04482
Rohit Jha <rohitj.ug19.cs@nitp.ac.in>
2022-02-13 01:01:04 +0530

    Added Backend for Blog feature

--- PhotoHub/Hub/admin.py
+++ PhotoHub/Hub/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Customer, Photographer, Appointment
+from .models import Customer, Photographer, Appointment, Blog, City
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Photographer)
 admin.site.register(Appointment)
-
+admin.site.register(Blog)
+admin.site.register(City)
